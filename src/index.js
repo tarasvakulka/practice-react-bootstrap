36728d76b541b6a291fbdcd10b348dd6a0de9b30
Taras <vakulkataras@gmail.com>
2018-01-20 20:53:55 +0200

    Delete comments

--- src/index.js
+++ src/index.js
@@ -11,27 +11,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 let store = createStore(products, composeWithDevTools(applyMiddleware(thunk)));
 
-//store.dispatch({ type: 'RECEIVE_PRODUCTS', payload: data_products.products })
-
 ReactDOM.render(<Provider store={store}><HashRouter><Route path="/" component={App}/></HashRouter></Provider>, document.getElementById('root'));
 registerServiceWorker();
-
-// import { createStore } from 'redux';
-
-// function productsList(state = [], action) {
-//     if (action.type === 'RECEIVE_PRODUCTS') {
-//         return [
-//             ...state,
-//             action.payload
-//         ];
-//     }
-//     return state;
-// }
-
-// const store = createStore(productsList);
-
-// store.subscribe(() => {
-//     console.log('subscribe', store.getState());
-// })
-
-// store.dispatch({ type: 'RECEIVE_PRODUCTS', payload: [1,2,3] })
